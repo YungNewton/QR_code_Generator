6bc6db5f67a1003e3608f556312aa58b1dfa953d
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-15 13:13:17 +0100

    commit

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
     {"message": "Enter URL for QR_code generation: ", "name": "URL"}
   ])
   .then((answers) => {
     const ans = answers.URL;
+    var qr_svg = qr.image(ans);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile("URL.txt" , ans, err =>{
+        if (err) throw err;
+        console.log("The file has been saved");
+    })
   })
   .catch((error) => {
     if (error.isTtyError) {
